4da4e03df115808e66838ce86193baf835fcb3ba
ZacLanghorne <zac.langhone@astrazeneca.com>
2022-01-27 16:11:17 +0000

    Removed unused imports

--- src/MapChart.js
+++ src/MapChart.js
@@ -1,10 +1,8 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 import {
   ComposableMap,
   Geographies,
   Geography,
-  Sphere,
-  Graticule,
   ZoomableGroup
 } from "react-simple-maps";
 
@@ -25,47 +23,47 @@ const MapChart = (props) => {
         <b>{countryName}</b>
         <p>Population ({popYear}): {population.toLocaleString()}</p>
       </div>)
-    
+
     props.setTooltipContent(content);
   }
 
   return (
-    <ComposableMap 
-      projection="geoMercator" 
+    <ComposableMap
+      projection="geoMercator"
       width={vw}
-      height={vh-50}
-      style={{paddingTop: 50}}
+      height={vh - 50}
+      style={{ paddingTop: 50 }}
       data-tip=""
     >
       <ZoomableGroup zoom={1}>
-      {(
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const d = props.data.find((s) => s === geo.properties.ISO_A3);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  stroke="#A1A1A1"
-                  onMouseEnter={() => setTooltip(geo)}
-                  onMouseLeave={() => props.setTooltipContent("")}
-                  style={{
-                    default: {
-                      fill: d ? "#76BA1B" : "#D6D6DA",
-                      outline: "none"
-                    },
-                    hover: {
-                      fill:  d ? "#7DC916" : "#e4e4e4",
-                      outline: "none"
-                    }
-                  }}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
+        {(
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const d = props.data.find((s) => s === geo.properties.ISO_A3);
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    stroke="#A1A1A1"
+                    onMouseEnter={() => setTooltip(geo)}
+                    onMouseLeave={() => props.setTooltipContent("")}
+                    style={{
+                      default: {
+                        fill: d ? "#76BA1B" : "#D6D6DA",
+                        outline: "none"
+                      },
+                      hover: {
+                        fill: d ? "#7DC916" : "#e4e4e4",
+                        outline: "none"
+                      }
+                    }}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        )}
       </ZoomableGroup>
     </ComposableMap>
   );
