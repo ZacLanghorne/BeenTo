b4e8960826ab164ff0ecd494849608c0c53e498f
ZacLanghorne <zac.langhone@astrazeneca.com>
2022-01-28 11:06:30 +0000

    Added bootstrap imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Parent from './Parent'
+import Parent from './Parent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
